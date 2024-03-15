3e283fc7aeecf690b15923397a6297d37f4afa36
Carlos Rises <72363307+carlosrises@users.noreply.github.com>
2024-03-15 18:55:04 +0000

    atualiza a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Denise, eu amo você! Se você me ama, dá uma risadinha! rsrs</h1>
+    return <h1>Denise, eu amo você! Se você me ama, dá uma risadinha! 😎 rsrs</h1>
 }
 
 export default Home;

bf6eb6b7ccbd71c31d812998506cfe9700248995
Carlos Rises <72363307+carlosrises@users.noreply.github.com>
2024-03-22 19:24:00 +0000

    finish `/api/ve/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 AS sum");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ status: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW SERVER_VERSION;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnnectionsResult = await database.query(
+    "SHOW MAX_CONNECTIONS;",
+  );
+  const databaseMaxConnnectionsValue =
+    databaseMaxConnnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependences: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnnectionsValue),
+        opened_connections: parseInt(openedConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
